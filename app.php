7ada6a2ff569846057aa333aa2bb47ad94af449e
Jody Mickey <jody.mickey@gmail.com>
2012-07-19 14:14:35 -0400

    added php templating engine

--- app.php
+++ app.php
@@ -1,9 +1,27 @@
 <?php
 
-$app = new Silex\Application();
+error_reporting(E_ALL ^ E_NOTICE);
+ini_set('display_errors', 'on');
 
-$app->get('/', function() {
-    return "<h1>Barebones Silex App</h1>";
+use Symfony\Component\Templating\PhpEngine;
+use Symfony\Component\Templating\TemplateNameParser;
+use Symfony\Component\Templating\Loader\FilesystemLoader;
+use Symfony\Component\Templating\Helper\SlotsHelper;
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// set up templating engine
+$app['view'] = $app->share(function() {
+    $loader = new FilesystemLoader(__DIR__ . '/views/%name%');
+    $view = new PhpEngine(new TemplateNameParser(), $loader);
+    $view->set(new SlotsHelper());
+
+    return $view;
+});
+
+$app->get('/', function(Silex\Application $app) {
+    return $app['view']->render('index.phtml');
 });
 
 $app->run();
